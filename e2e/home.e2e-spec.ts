ed4a58dc52248dfd0e05e18cc99c236f90f95253
CireEsma <eric.ms0703@gmail.com>
2025-12-31 00:38:45 +0100

    feat: update e2e tests

--- e2e/home.e2e-spec.ts
+++ e2e/home.e2e-spec.ts
@@ -8,4 +8,24 @@ test.describe('Home page', () => {
   test('should display a title', async ({ page }) => {
     await expect(page.locator('h1')).toContainText('Ponyracer');
   });
+
+  test('should display a navbar', async ({ page }) => {
+    await expect(page.locator('.navbar-brand')).toContainText('PonyRacer');
+    await expect(page.locator('.nav-link')).toContainText('Races');
+  });
+
+  test('should display a navbar collapsed on small screen', async ({ page }) => {
+    await page.setViewportSize({ width: 375, height: 667 });
+    const navbarBrand = page.locator('.navbar-brand');
+    await expect(navbarBrand).toBeVisible();
+    const navbarLink = page.locator('.nav-link');
+    await expect(navbarLink).not.toBeVisible();
+
+    const navbarToggler = page.locator('.navbar-toggler');
+    await navbarToggler.click();
+    await expect(navbarLink).toBeVisible();
+
+    await navbarToggler.click();
+    await expect(navbarLink).not.toBeVisible();
+  });
 });
