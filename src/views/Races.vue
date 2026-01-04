ee0857ac7b641034b1e9bee17ce746cea2d3276d
CireEsma <eric.ms0703@gmail.com>
2026-01-04 19:56:08 +0100

    feat: create Races view

---
+++ src/views/Races.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- eslint-disable-next-line vue/require-v-for-key -->
+  <div v-for="race in races">
+    <h2>{{ race.name }}</h2>
+  </div>
+</template>
+<script lang="ts">
+import { RaceModel } from '@/models/RaceModel';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'Races',
+
+  setup() {
+    const races = ref<Array<RaceModel>>([{ name: 'Lyon' }, { name: 'London' }]);
+
+    return {
+      races
+    };
+  }
+});
+</script>
